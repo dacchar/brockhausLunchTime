6f4df97760332bf7276ad2d4ff773554aff8c040
Dmitry Charushnikov <Dmitry.Charushnikov@msg.group>
2025-07-24 14:29:39 +0200

    init project

---
+++ brockhaus-lunch-time-backend/src/test/java/com/example/brockhaus_lunch_time_backend/BrockhausLunchTimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brockhaus_lunch_time_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrockhausLunchTimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
