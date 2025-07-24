b300553ca55b48b1fc0213311e094892840df5e6
Dmitry Charushnikov <Dmitry.Charushnikov@msg.group>
2025-07-24 13:07:15 +0200

    fix: remove unneccessary import

--- brockhaus-lunch-time-frontend/src/app/app.routes.ts
+++ brockhaus-lunch-time-frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { OrderListComponent } from './components/order-list-component/order-list-component';
 import { OrderComponent } from './components/order-component/order-component';
 import { DashboardComponent } from './components/dashboard-component/dashboard-component';
 
